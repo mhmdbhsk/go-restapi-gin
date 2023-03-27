180ebba4278fe38c8a368a76369775426a4066ea
mhmdbhsk <muhammadbhaska0@gmail.com>
2023-03-27 13:47:11 +0700

    feat: initial commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/mhmhdbhsk/go-restapi-gin/controllers/productController"
+	"github.com/mhmhdbhsk/go-restapi-gin/models"
+)
+
+func main() {
+	r := gin.Default()
+	models.ConnectDatabase()
+
+	r.GET("/api/products", productController.Index)
+	r.GET("/api/product/:id", productController.Show)
+	r.POST("/api/product", productController.Create)
+	r.PUT("/api/product/:id", productController.Update)
+	r.DELETE("/api/product", productController.Delete)
+
+	r.Run(":8800")
+}
